18844adcb3ecec9e12370ece8f4a8eddf84609df
Jean Lopes <jean.lopes@grupomfx.com>
2023-03-09 00:19:22 -0300

    Inserção do script que monta os cards dinamicamente, com base no array de objetos enviados

--- main.js
+++ main.js
@@ -1,8 +1,58 @@
-document.addEventListener("DOMContentLoaded", function(e) {
-    
+document.addEventListener("DOMContentLoaded", function (e) {
+    const infosCard = [
+        {
+            id: 2322,
+            description: "Coca Cola Zero Açúcar",
+            img: "img/cocacola2.png",
+            price: "R$ 5,00"
+        }, {
+            id: 2323,
+            description: "Coca Cola Diet",
+            img: "img/cocacola3.png",
+            price: "R$ 6,00"
+        },
+        {
+            id: 2324,
+            description: "Coca Cola Coffee",
+            img: "img/cocacola4.png",
+            price: "R$ 7,50"
+        },
+        {
+            id: 2325,
+            description: "Coca Cola Vannila",
+            img: "img/cocacola5.png",
+            price: "R$ 6,00"
+        },
+        {
+            id: 2326,
+            description: "Coca Cola Energy",
+            img: "img/cocacola6.png",
+            price: "R$ 10,00"
+        }
+    ]
+
+    cardClone(infosCard)
 });
 
-var swiper = new Swiper(".mySwiper", {
+var header = document.querySelector('header');
+
+window.onscroll = function () {
+    if (window.pageYOffset > 80) {
+        header.style.position = "fixed";
+        header.style.top = 0;
+        header.style.background = "rgba(0,0,0,0.6)";
+        header.style.backdropFilter = "saturate(180%) blur(20px)"
+        header.style.zIndex = "100000"
+        header.style.height = "80px"
+    } else {
+        header.style.position = "absolute";
+        header.style.top = 80;
+        header.style.background = "transparent";
+        header.style.backdropFilter = "none"
+    }
+}
+
+let swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
@@ -17,7 +67,21 @@ var swiper = new Swiper(".mySwiper", {
     loop: true
 });
 
-function toggleMenu(){
+function cardClone(infosCloneCard) {
+    let contentCardSwiper = document.querySelector(".card-swiper-content")
+    let cardShopping = document.querySelector('.cards')
+
+    infosCloneCard.map(infos => {
+        const newCard = cardShopping.cloneNode(true)
+        newCard.setAttribute("id", infos.id)
+        newCard.querySelector("img").src = infos.img
+        newCard.querySelector("h4").innerHTML = infos.description
+        newCard.querySelector(".price").innerHTML = infos.price
+        contentCardSwiper.appendChild(newCard)
+    })
+}
+
+function toggleMenu() {
     const menuToggle = document.querySelector(".toggle");
     const navigation = document.querySelector(".navigation")
     menuToggle.classList.toggle("active")
@@ -25,7 +89,7 @@ function toggleMenu(){
 }
 
 
-var swiperCard = new Swiper(".mySwiperCard", {
+let swiperCard = new Swiper(".mySwiperCard", {
     slidesPerView: 3,
     spaceBetween: 30,
     freeMode: true,
